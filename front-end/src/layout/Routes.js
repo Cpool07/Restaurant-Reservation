de101078e25596c2f31e2451d38af4d12ec40ba3
Michael Claypool <michaelrclaypool@gmail.com>
2022-10-31 12:34:14 -0400

    US-02 complete

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -1,9 +1,9 @@
 import React from "react";
-
 import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
+import NewReservation from "../reservations/NewReservation";
 import useQuery from "../utils/useQuery";
 
 
@@ -17,6 +17,7 @@ import useQuery from "../utils/useQuery";
 function Routes() {
   const query = useQuery();
   const date = query.get("date");
+  
   return (
     <Switch>
       <Route exact={true} path="/">
@@ -25,8 +26,11 @@ function Routes() {
       <Route exact={true} path="/reservations">
         <Redirect to={"/dashboard"} />
       </Route>
+      <Route exact={true} path="/reservations/new">
+        <NewReservation />
+      </Route>
       <Route path="/dashboard">
-        <Dashboard date={date ? date: today()} />
+        <Dashboard date={date || today()} />
       </Route>
       <Route>
         <NotFound />
